9401185edddae6d0fccf727e3f77894702c7f6c0
RuotG123 <ruotgal123@gmail.com>
2025-08-06 21:45:37 -0500

    Make fixes to some files and changing logic

---
+++ login_system/apps.py
@@ -0,0 +1,7 @@
+ffrom django.apps import AppConfig
+
+
+class LoginSystemConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'login_system'
+    verbose_name = 'Login System'
---
+++ login_system/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.views import View
+
+
+class LoginView(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('main_menu')
+        return render(request, 'login_system/login.html')
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main_menu')
+        return render(request, 'login_system/login.html', {'error': 'Invalid credentials'})
+
+
+@login_required
+def main_menu_view(request):
+    return render(request, 'login_system/main_menu.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
